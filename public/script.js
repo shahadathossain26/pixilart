6ccb877fd2518585d79b295416f57f1eadbf886a
shahadathossain26 <shiimran172372@gmail.com>
2023-03-12 20:18:48 +0600

    circle and triangle functionality added

--- public/script.js
+++ public/script.js
@@ -32,6 +32,25 @@ drawRectangle = (e) => {
 
 }
 
+var drawCircle = (e) => {
+    ctx.beginPath();
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2))
+    ctx.arc(prevMouseX, prevMouseY, radius, 50, 0, 2 * Math.PI);
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+
+}
+
+var drawTriangle = (e) => {
+    ctx.beginPath();
+    ctx.moveTo(prevMouseX, prevMouseY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+    ctx.closePath();
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+
+
+}
+
 drawing = (e) => {
     if (!isDrawing) return;
     ctx.putImageData(snapshot, 0, 0)
@@ -41,6 +60,12 @@ drawing = (e) => {
     }
     else if (selectedTool === 'rectangle') {
         drawRectangle(e);
+    }
+    else if (selectedTool === 'circle') {
+        drawCircle(e);
+    }
+    else {
+        drawTriangle(e)
     }
 }
 
